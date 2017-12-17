918f0de2d8e127cc22766ba4e78b799649da5e91
Andrew Doyle <doyle.andrew@gmail.com>
2017-12-17 14:12:27 -0500

    Deep stuff working

--- graph_smarts.py
+++ graph_smarts.py
@@ -18,6 +18,8 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
 from deep_stuff import deep_mlp
+from keras.callbacks import ModelCheckpoint
+from keras.models import load_model
 
 
 root_dir = 'E:/brains/graphs/'
@@ -49,13 +51,15 @@ linreg = LinearRegression(normalize=True)
 lasso = Lasso(fit_intercept=True, normalize=True)
 
 classifiers = [pls, linreg, lasso]
-classifier_names = ['PLS', 'Regression', 'Lasso']
+classifier_names = ['LR', 'PLS', 'Lasso']
 
 r = {}
 for graphtype in graphtypes:
     r[graphtype] = {}
     for name in classifier_names:
         r[graphtype][name] = []
+
+    r[graphtype]['deep'] = []
 
 for graphtype, n_roi in zip(graphtypes, rois):
     print('Running analysis for: ', graphtype)
@@ -114,15 +118,20 @@ for graphtype, n_roi in zip(graphtypes, rois):
         # print('nans:', np.sum(np.isnan(y_train)))
         # print('infs:', np.sum(np.isinf(y_train)))
 
+
+        model_checkpoint = ModelCheckpoint(root_dir + 'best_model.hdf5', monitor="val_loss", verbose=0, save_best_only=True, save_weights_only=False, mode='min')
+
         model = deep_mlp(n_roi)
         model.compile('adam', 'mse', metrics=['accuracy'])
-        model.fit(x_train, y_train, epochs=100, validation_split=0.1)
+        model.fit(x_train, y_train, epochs=1000, validation_split=0.1, callbacks=[model_checkpoint])
+
+        model.load_weights(root_dir + 'best_model.hdf5')
 
         predictions = model.predict(x_test)
         actual = y_test
 
-        r[graphtype]['deep'] = []
-        r[graphtype]['deep'].append(r2_score(actual, predictions))
+        r2 = r2_score(actual, predictions)
+        r[graphtype]['deep'].append(r2)
 
         for classifier, name in zip(classifiers, classifier_names):
             classifier.fit(x_train, y_train)
@@ -131,7 +140,7 @@ for graphtype, n_roi in zip(graphtypes, rois):
 
 for graphtype in graphtypes:
     print('Parcelation:', graphtype)
-    for name in classifier_names:
+    for name in classifier_names + ['deep']:
         print(name, np.mean(r[graphtype][name]), np.std(r[graphtype][name]))
 
 scores = []
@@ -139,7 +148,7 @@ score_labels = []
 
 
 for graphtype in graphtypes:
-    for name in [classifier_names + 'deep']:
+    for name in classifier_names + ['deep']:
         scores.append(r[graphtype][name])
         score_labels.append(name)
 
@@ -150,6 +159,7 @@ bplot = plt.boxplot(scores, patch_artist=True, zorder=3)
 plt.xticks(np.arange(1, len(scores)+1), score_labels, rotation=0, horizontalalignment='center', fontsize=20)
 plt.grid(zorder=0)
 plt.xlim(0, len(scores) + 1)
+plt.ylim(0, 1)
 
 colors = ['pink', 'red', 'darkred', 'firebrick', 'pink', 'red', 'darkred', 'firebrick', 'pink', 'red', 'darkred', 'firebrick']
 
